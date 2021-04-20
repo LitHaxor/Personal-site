5f4adf3453f522f81de9c06bffcc46dbce561eaf
Hasan Ahamed <officials.hasan@gmail.com>
2021-04-20 16:23:32 +0600

    refactoring

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,32 +29,39 @@ const NavItem:FunctionComponent<Items> = ({
 
 
 const DarkButton = ({theme, toggleTheme}) =>{
-   if(theme === 'dark' ){
-       return (
-         
-              <FiSun onClick={toggleTheme} className="h-6 w-6 text-yellow-600 inline-block cursor-pointer"/>
-            
-       )
-    }
-   return (
-           <FaMoon onClick={toggleTheme} className="h-6 w-6 inline-block cursor-pointer text-black"/>
-   )
+    return(
+        <button onClick={toggleTheme}>
+            {
+                theme === 'dark' ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                ): (
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-400" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
+                    </svg>
+                )
+            }
+        </button>
+    )
 }
+
+ 
 
 const Navbar = () => {
     const {theme, setTheme} = useTheme()
-    
-    const toggleTheme = () =>{
-       setTheme(theme === 'light' ? 'dark' : 'light')
-    }
     const {pathname} = useRouter();
     const [active, setActive] = React.useState<string>();
+    const toggleTheme = () =>{
+        theme === 'light' ? setTheme('dark'): setTheme('light');
+    }  
     useEffect(
         ()=>{
+            console.log(theme)
             if(pathname ==="/") setActive('About')
             else if (pathname==="/Portfolio") setActive('Portfolio')
             else if(pathname==="/Resume ") setActive('Resume')
-        },[])
+        },[theme])
 
     useEffect(()=>{
         console.log('theme:', theme);
@@ -80,9 +87,7 @@ const Navbar = () => {
                     setActive={setActive} 
                     name="Resume"
                     route="/Resume"/>
-                <div className="inline-block">
                     <DarkButton theme={theme} toggleTheme={toggleTheme}/>
-                </div>
             </div>
         </div>
     )
